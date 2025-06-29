f432a8e062fc1b29693f8a5602d32a8a96ce5246
vladik <martynenkovlad97@gmail.com>
2025-06-29 14:21:28 +0000

    4 try

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,8 +1,10 @@
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = False
